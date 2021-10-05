bdc9bf79c04002c5db2cdb0c71ab3f3e36d3b76b
benber86 <benoitb@tutanota.de>
2021-10-05 15:25:36 +1100

    Price Token A in Token B util function. Fix ABI imports

--- packages/utils/pricing.ts
+++ packages/utils/pricing.ts
@@ -1,4 +1,4 @@
-import { Address, BigDecimal } from '@graphprotocol/graph-ts'
+import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import {
   ADDRESS_ZERO,
   BIG_DECIMAL_1E18,
@@ -7,11 +7,13 @@ import {
   BIG_DECIMAL_ZERO,
   SUSHI_FACTORY_ADDRESS,
   SUSHISWAP_WETH_USDT_PAIR_ADDRESS,
-  USDT_ADDRESS,
-  WETH_ADDRESS,
+  USDT_ADDRESS, WBTC_ADDRESS,
+  WETH_ADDRESS
 } from 'const'
-import { Factory } from 'locker/generated/CvxLocker/Factory'
-import { Pair } from 'locker/generated/CvxLocker/Pair'
+import { Factory } from 'curve-pools/generated/Booster/Factory'
+import { Pair } from 'curve-pools/generated/Booster/Pair'
+import { ERC20 } from 'curve-pools/generated/Booster/ERC20'
+import { exponentToBigDecimal } from './maths'
 
 export function getEthRate(token: Address): BigDecimal {
   let eth = BIG_DECIMAL_ONE
@@ -39,24 +41,40 @@ export function getEthRate(token: Address): BigDecimal {
   return eth
 }
 
+export function getDecimals(token: Address): BigInt {
+  const tokenContract = ERC20.bind(token)
+  const decimalsResult = tokenContract.try_decimals()
+  return decimalsResult.reverted ? BigInt.fromI32(18) : BigInt.fromI32(decimalsResult.value)
+}
+
+// Computes the value of one unit of Token A in units of Token B
+// Only works if both tokens have an ETH pair on Sushi
+export function getTokenAValueInTokenB(tokenA: Address, tokenB: Address): BigDecimal {
+  const decimalsA = getDecimals(tokenA)
+  const decimalsB = getDecimals(tokenB)
+  const ethRateA = getEthRate(tokenA).times(BIG_DECIMAL_1E18)
+  const ethRateB = getEthRate(tokenB).times(BIG_DECIMAL_1E18)
+
+  return (ethRateA.div(ethRateB)).times(exponentToBigDecimal(decimalsA)).div(exponentToBigDecimal(decimalsB))
+}
+
 export function getUSDRate(token: Address): BigDecimal {
   const usdt = BIG_DECIMAL_ONE
 
   if (token != USDT_ADDRESS) {
-    const tokenPriceETH = getEthRate(token)
-
-    const pair = Pair.bind(SUSHISWAP_WETH_USDT_PAIR_ADDRESS)
-
-    const reserves = pair.getReserves()
-
-    const reserve0 = reserves.value0.toBigDecimal().times(BIG_DECIMAL_1E18)
-
-    const reserve1 = reserves.value1.toBigDecimal().times(BIG_DECIMAL_1E18)
-
-    const ethPriceUSD = reserve1.div(reserve0).div(BIG_DECIMAL_1E6).times(BIG_DECIMAL_1E18)
-
-    return ethPriceUSD.times(tokenPriceETH)
+    return getTokenAValueInTokenB(token, USDT_ADDRESS)
   }
 
   return usdt
+}
+
+
+export function getBTCRate(token: Address): BigDecimal {
+  const wbtc = BIG_DECIMAL_ONE
+
+  if (token != WBTC_ADDRESS) {
+    return getTokenAValueInTokenB(token, WBTC_ADDRESS)
+  }
+
+  return wbtc
 }
