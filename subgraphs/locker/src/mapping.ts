ca3426b122ffc8b7bb54b348ae264906ed1472c1
benber86 <benoitb@tutanota.de>
2021-10-07 20:18:53 +1100

    Rename getUSDRate to getUsdRate

--- subgraphs/locker/src/mapping.ts
+++ subgraphs/locker/src/mapping.ts
@@ -12,7 +12,7 @@ import {
 } from "./services/events";
 import {getOrCreateToken} from "./services/tokens";
 import {CVX_ADDRESS} from "const";
-import {getUSDRate} from "utils/pricing"
+import {getUsdRate} from "utils/pricing"
 import {Address} from '@graphprotocol/graph-ts';
 import {exponentToBigDecimal} from "utils/maths";
 
@@ -24,7 +24,7 @@ export function handleKickReward(event: KickReward): void {
   const reward = new Reward(getEventId(event));
   reward.user = user.id;
   reward.amount = event.params._reward;
-  reward.amountUSD = getUSDRate(CVX_ADDRESS).times(reward.amount.toBigDecimal()
+  reward.amountUSD = getUsdRate(CVX_ADDRESS).times(reward.amount.toBigDecimal()
       .div(exponentToBigDecimal(cvx.decimals)));
   reward.token = cvx.id;
   reward.time = event.block.timestamp;
@@ -45,7 +45,7 @@ export function handleRewardPaid(event: RewardPaid): void {
   reward.user = user.id;
   reward.amount = event.params._reward;
   const token = getOrCreateToken(event.params._rewardsToken);
-  reward.amountUSD = getUSDRate(Address.fromString(token.id)).times(reward.amount.toBigDecimal()
+  reward.amountUSD = getUsdRate(Address.fromString(token.id)).times(reward.amount.toBigDecimal()
       .div(exponentToBigDecimal(token.decimals)));
   reward.token = token.id;
   reward.time = event.block.timestamp;
@@ -63,7 +63,7 @@ export function handleStaked(event: Staked): void {
   const lock = new Lock(getEventId(event));
   lock.user = user.id;
   lock.lockAmount = event.params._lockedAmount;
-  lock.amountUSD = getUSDRate(CVX_ADDRESS).times(lock.lockAmount.toBigDecimal().div(exponentToBigDecimal(cvx.decimals)));
+  lock.amountUSD = getUsdRate(CVX_ADDRESS).times(lock.lockAmount.toBigDecimal().div(exponentToBigDecimal(cvx.decimals)));
   lock.boostedAmount = event.params._boostedAmount;
   lock.time = event.block.timestamp;
   user.totalLocked = user.totalLocked.plus(lock.lockAmount);
@@ -79,7 +79,7 @@ export function handleWithdrawn(event: Withdrawn): void {
   const withdrawal = new Withdrawal(getEventId(event));
   withdrawal.user = user.id;
   withdrawal.amount = event.params._amount;
-  withdrawal.amountUSD = getUSDRate(CVX_ADDRESS).times(withdrawal.amount.toBigDecimal().div(exponentToBigDecimal(cvx.decimals)));
+  withdrawal.amountUSD = getUsdRate(CVX_ADDRESS).times(withdrawal.amount.toBigDecimal().div(exponentToBigDecimal(cvx.decimals)));
   withdrawal.time = event.block.timestamp;
   user.totalLocked = user.totalLocked.minus(withdrawal.amount);
   user.save();
