41c4eb5a7d918d5c14361cfae8c7345c2f75bf03
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2021-11-20 21:32:46 +0100

    init: mapping skeleton

---
+++ subgraphs/tricrypto-eth/src/mapping.ts
@@ -0,0 +1,109 @@
+import { TricryptoSnapshot } from '../generated/schema'
+import {
+    TRICRYPTO2_ETH_POOL_ADDRESS,
+    TRICRYPTO2_LP_ADDRESS
+} from "../../../packages/constants";
+
+
+// get pool parameters each time a token exchange takes place
+export function handleTokenExchange(event: TokenExchange): void {
+
+    // get event values
+
+
+    // get pool coin balances
+
+
+    // get virtual price
+
+
+    // get price oracle values for btc, eth
+
+
+    // get price scale values for btc, eth
+
+
+    // get swap fee
+
+
+    // get swap rates
+
+}
+
+
+// get pool parameters each time a token exchange takes place
+export function handleAddLiquidity(event: TokenExchange): void {
+
+    // get event values
+
+
+    // get pool coin balances
+
+
+    // get virtual price
+
+
+    // get price oracle values for btc, eth
+
+
+    // get price scale values for btc, eth
+
+
+    // get swap fee
+
+
+    // get swap rates
+
+}
+
+
+// get pool parameters each time a token exchange takes place
+export function handleRemoveLiquidity(event: TokenExchange): void {
+
+    // get event values
+
+
+    // get pool coin balances
+
+
+    // get virtual price
+
+
+    // get price oracle values for btc, eth
+
+
+    // get price scale values for btc, eth
+
+
+    // get swap fee
+
+
+    // get swap rates
+
+}
+
+
+// get pool parameters each time a token exchange takes place
+export function handleClaimAdminFee(event: TokenExchange): void {
+
+    // get event values
+
+
+    // get pool coin balances
+
+
+    // get virtual price
+
+
+    // get price oracle values for btc, eth
+
+
+    // get price scale values for btc, eth
+
+
+    // get swap fee
+
+
+    // get swap rates
+
+}
